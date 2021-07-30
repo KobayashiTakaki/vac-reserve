12d2a900cbcefd2a8e34263dd755c4f59a2d8451
kobayashitakaki <o.share.thief@gmail.com>
2021-07-30 23:24:49 +0900

    feat: throttling notification

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import urllib.parse
-from datetime import date
+from datetime import date, datetime, timedelta, timezone
 import json
 import requests
 from dotenv import load_dotenv
@@ -9,6 +9,7 @@ load_dotenv()
 LINE_ACCESS_TOKEN = os.getenv('LINE_ACCESS_TOKEN')
 ORGANIZATION_ID = os.getenv('ORGANIZATION_ID')
 LOGIN_URL= os.getenv('LOGIN_URL')
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 def create_line_message(text):
     url = 'https://api.line.me/v2/bot/message/broadcast'
@@ -56,7 +57,30 @@ def display_format_time_slot(t):
     ret += '(2回目予定: {} {}-{})'.format(next_d, next_start_time, next_end_time)
     return ret
 
+def did_notified_recently():
+    dir = os.path.dirname(__file__)
+    log_file_path = os.path.join(dir, 'last_notified.log')
+    if not os.path.isfile(log_file_path):
+        return False
+    with open(log_file_path, mode='r') as f:
+        s = f.read()
+    last_notified = datetime.strptime(s, DATETIME_FORMAT)
+    now = datetime.now()
+    if now - last_notified < timedelta(hours=1):
+        return True
+    else:
+        return False
+
+def save_last_notified():
+    dir = os.path.dirname(__file__)
+    log_file_path = os.path.join(dir, 'last_notified.log')
+    with open(log_file_path, mode='w') as f:
+        now = datetime.now(tz=timezone(offset=timedelta(hours=9)))
+        f.write(now.strftime(DATETIME_FORMAT))
+
 if __name__ == '__main__':
+    if did_notified_recently():
+        exit()
     time_slots = fetch_time_slots()
     available_time_slot = [t for t in time_slots if is_time_slot_available(t)]
     if len(available_time_slot) == 0:
@@ -66,3 +90,4 @@ if __name__ == '__main__':
     for t in available_time_slot:
         message += display_format_time_slot(t)
     create_line_message(message)
+    save_last_notified()
